b6d43cfade64f486ecdaa3c6cfb877018ba19ca0
Jean-Charles Sisk <jasisk@gmail.com>
2013-02-28 15:49:05 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,69 @@
+var domready = require('domready');
+var reconnect = require('reconnect');
+var shoe = require('shoe');
+var dnode = require('dnode');
+
+var cookie = {
+  get: function (key) {
+    return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+  },
+  set: function (key, value) {
+    if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) { return; }
+    document.cookie = escape(key) + "=" + escape(value);
+  }
+};
+
+domready(function(){
+  var textbox = document.createElement("div");
+  var iframe = document.createElement("iframe");
+  textbox.style.display = "none";
+  iframe.style.display = "none";
+  textbox.style.height = window.innerHeight + "px";
+  iframe.style.height = window.innerHeight + "px";
+
+  window.addEventListener("resize", function(){
+    textbox.style.height = window.innerHeight + "px";
+    iframe.style.height = window.innerHeight + "px";    
+  }, false);
+  document.body.appendChild(textbox);
+  document.body.appendChild(iframe);
+
+  var uuid = cookie.get('uuid');
+  var stream = shoe('/command');
+
+  reconnect(function(stream){
+    var d = dnode({
+      log: function(logs, cb){
+        console.log(logs);
+        if (cb) { cb(true); }
+      },
+      reload: function(cb){
+        if (cb) { cb("Reloading."); }
+        window.location.reload();
+      },
+      iframe: function(src, cb){
+        textbox.style.display = "none";
+        iframe.style.display = "block";
+        iframe.setAttribute("src", src);
+        if (cb) { cb("Set iFrame."); }
+      },
+      text: function(text, cb){
+        iframe.style.display = "none";
+        textbox.style.display = "block";
+        textbox.textContent = text;
+        if (cb) { cb("Set text."); }
+      }
+    });
+    d.on('remote', function(remote){
+      window.remote = remote;
+      remote.register(uuid, function(id, md){
+        if (window.location.hash && window.location.hash.length > 1) {
+          remote.sessionSet("name", window.location.hash.substr(1));
+          history.replaceState({}, "", window.location.href.substring(0,window.location.href.indexOf("#")));
+        }
+        cookie.set('uuid', id);
+      });
+    });
+    d.pipe(stream).pipe(d);
+  }).connect('/command');
+});
---
+++ index.js
@@ -0,0 +1,119 @@
+var http = require('http');
+var util = require('util');
+var ecstatic = require('ecstatic')(__dirname + '/public');
+var shoe = require('shoe');
+var dnode = require('dnode');
+var uuid = require('node-uuid');
+var levelup = require('levelup');
+
+var db = levelup('./monitors');
+var clients = {};
+var remotes = {};
+
+var server = http.createServer(ecstatic);
+server.listen(8008);
+
+var sock = shoe(function(stream){
+  var metadata = {}, id;
+
+  var createId = function(cb){
+    var id = uuid.v4();
+    var md = {
+      'created_at': Date.now()
+    };
+    db.put(id, JSON.stringify(md), function(err){
+      cb({
+        id: id,
+        metadata: md
+      });
+    });
+  };
+
+  var identifier = function(){
+    return metadata.name ? id + " (" + metadata.name + ")" : id;
+  };
+
+  var d = dnode({
+    sessionSet: function(k, v){
+      metadata[k] = v;
+      db.put(id, JSON.stringify(metadata));
+      console.log("%s : SET %s TO %s", identifier(), k, v);
+    },
+    sessionGet: function(k, cb){
+      cb(metadata[k]);
+    },
+    getClients: function(cb){
+      cb(clients);
+    },
+    sendClient: function(client, method, args, cb){
+      if (client === "*") {
+        client = Object.keys(remotes);
+      } else {
+        if (remotes[client]) {
+          client = [client];
+        }
+      }
+
+      if (!util.isArray(args)) {
+        if (typeof args === 'function') {
+          cb = args;
+        }
+        args = [];
+      }
+
+      client.forEach(function(v){
+        var remote = remotes[v];
+        var argsClone = args.slice(0);
+        if (cb) {
+          argsClone.push(function(){
+            cb.apply(this, Array.prototype.concat.apply([v], arguments));
+          });        
+        }
+        remote[method].apply(remote, argsClone);
+      });
+    },
+    register: function(s, cb){
+      var md = {}, cid;
+      var finalize = function(cId, cMeta){
+        id = cId;
+        if (typeof cMeta === "string") {
+          cMeta = JSON.parse(cMeta);
+        }
+        metadata = cMeta;
+        console.log("%s : %s", identifier(), "CONNECTED");
+        clients[id] = metadata;
+        d.emit("registered", id);
+        cb(id, metadata);
+      };
+      var callback = function(err, md){
+        if (err) {
+          createId(function(cid){
+            finalize(cid.id, cid.metadata);
+          });
+        } else {
+          finalize(s, md);
+        }
+      };
+      if (s) {
+        db.get(s, callback);
+      } else {
+        callback(true);
+      }
+    }
+  });
+  d.on('remote', function(remote){
+    d.on("registered", function(id){
+      remotes[id] = remote;
+    });
+  });
+  d.pipe(stream).pipe(d);
+  stream.on('close', function(){
+    if (id && clients[id]) {
+      delete clients[id];
+      delete remotes[id];
+      console.log("%s : %s", identifier(), "DISCONNECTED");
+    }
+  });
+});
+
+sock.install(server, '/command');
